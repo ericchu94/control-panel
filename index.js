b79fe54ca46fef80765e82530dce46e00de08773
Eric Chu <eric@ericchu.net>
2016-02-08 21:11:52 -0500

    Add basic online tracker

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+'use strict';
+
 const Koa = require('koa');
 const router = require('koa-router')();
 const app = new Koa();
 
+app.context.ping = 'never';
+app.context.online = {};
+
 router.get('/', function (ctx, next) {
-  const ping = app.context.ping || 'never';
-  ctx.body = 'Last ping: ' + ping;
+  ctx.body = 'Last ping: ' + app.context.ping;
 });
 
 router.get('/ping', function (ctx, next) {
   const now = new Date();
   ctx.body = now;
-  app.context.ping = now;
+  ctx.app.context.ping = now;
+});
+
+router.get('/online', function (ctx, next) {
+  ctx.body = '';
+  const online = ctx.app.context.online;
+  for (const hostname in online) {
+    const lastUpdate = online[hostname];
+    const diff = new Date() - lastUpdate;
+    let status = 'offline';
+    if (diff < 60 * 1000) {
+      status = 'online';
+    }
+    ctx.body += hostname + ': ' + status;
+  }
+});
+
+router.put('/online/:hostname', function (ctx, next) {
+  const hostname = ctx.params.hostname;
+  ctx.app.context.online[hostname] = new Date();
+  ctx.status = 200;
 });
 
 app.use(router.routes());
